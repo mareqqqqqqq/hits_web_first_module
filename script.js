597ed34fc1ddc009a4c21ff1861ca4b394e2b770
pierreofcourse <kubplay123lol@gmail.com>
2026-02-13 19:08:42 +0700

    add_some_js

--- script.js
+++ script.js
@@ -18,3 +18,92 @@ function createBlock(x, y, color, id) {
 
 canvas.addEvent
 
+
+
+
+canvas.addEventListener('mousemove',e => {
+    if (!selected) 
+        return;
+
+    const pt = canvas.createSVGPoint();
+
+    pt.x = e.clientX;
+    pt.y = e.clientY;
+
+    const x = pt.x - offsetX;
+    const y = pt.y - offsetY;
+
+    selected.setAttribute(`transform`, `translate(${x},${y})`);
+})
+
+
+
+canvas.addEventListener('mouseup', e => {
+    if (!selected) return;
+
+    const selMatrix = selected.transform.baseVal.consolidate().matrix;
+    const selBBox = selected.getBBox();
+
+    const selX = selMatrix.e;
+    const selY = selMatrix.f;
+
+    const blocks = Array.from(document.querySelectorAll('.block'))
+        .filter(b => b !== selected);
+
+    blocks.forEach(block => {
+        const m = block.transform.baseVal.consolidate().matrix;
+        const bBox = block.getBBox();
+
+        const bx = m.e;
+        const by = m.f;
+
+        // расстояние между правым краем одного и левым краем другого
+        const dx = Math.abs((selX + selBBox.width) - bx);
+        const dy = Math.abs((selY + selBBox.height / 2) - (by + bBox.height / 2));
+
+        // если достаточно близко — прилипает
+        if (dx < 15 && dy < 15) {
+            const snapX = bx - selBBox.width;
+            const snapY = by;
+
+            selected.setAttribute(
+                'transform',
+                translate(${snapX}, ${snapY})
+            );
+        }
+    });
+
+    selected.style.cursor = 'grab';
+    selected = null;
+});
+
+// РАЗОБРАТЬ ЧТО ВЫШЕ ЗАВТРА ЖБШНО
+
+// e - типо event  
+
+canvas.addEventListener('mousedown', e => {
+    if (!e.target.classList.contains('block'))
+        return;
+
+    selected = e.target;
+
+    const pt = canvas.createSVGPoint();
+
+    pt.x = e.clientX;
+    pt.y = e.clientY;
+
+    const matrix = selected.transform.baseVal.consolidate().matrix;
+
+    offsetX = pt.x - matrix.e;
+    offsetY = pt.y - matrix.f;
+
+    selected.style.cursor = 'grabbing';
+})
+
+
+
+
+
+
+
+
